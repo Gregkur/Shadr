65622b68ec0068bad94b622d2e0a3c31b0021e11
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-12-30 17:34:02 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Palette from "./components/Palette";
 import seedColors from "./seedColors";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Palette {...seedColors[4]} />
-      <Palette {...seedColors[3]} />
-      <Palette {...seedColors[2]} />
-      <Palette {...seedColors[1]} />
     </div>
   );
 }
--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import "../styles/ColorBox.css";
 export default class ColorBox extends Component {
   render() {
+    const { name, background } = this.props;
     return (
-      <div className="ColorBox" style={{ background: this.props.background }}>
-        <span>{this.props.name}</span>
-        <span>MORE</span>
+      <div className="ColorBox" style={{ background }}>
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+        <span className="see-more">More</span>
       </div>
     );
   }
