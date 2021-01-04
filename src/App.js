467738f5811cb7faa6fa35a210eafe49edf7f4f7
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-01-04 16:37:37 +0100

    added newly functioning shades

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import "./App.css";
 function App() {
   console.log(generatePalette(seedColors[4]));
   return (
-    <div className="App">
-      <Palette {...seedColors[4]} />
+    <div>
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -3,8 +3,8 @@ import ColorBox from "./ColorBox";
 import "../styles/Palette.css";
 export default class Palette extends Component {
   render() {
-    const colorBoxes = this.props.colors.map((color) => (
-      <ColorBox background={color.color} name={color.name} key={color.color} />
+    const colorBoxes = this.props.palette.colors[500].map((color) => (
+      <ColorBox background={color.hex} name={color.name} key={color.hex} />
     ));
     return (
       <div className="Palette">
