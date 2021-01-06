ae71a65898ec64df249f1a5de87f573cf215a3df
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-01-06 16:16:47 +0100

    styling of minipalette finished"

--- src/components/MiniPalette.js
+++ src/components/MiniPalette.js
@@ -13,7 +13,7 @@ const styles = {
       cursor: "pointer",
     },
   },
-  colors: { backgroundColor: "grey" },
+  colors: { backgroundColor: "grey", height: "150px", width: "100%" },
   title: {
     display: "flex",
     justifyContent: "space-between",
@@ -25,12 +25,28 @@ const styles = {
     position: "relative",
   },
   emoji: { marginLeft: "0.5rem", fontSize: "1.5rem" },
+  miniColor: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    margin: "0 auto",
+    position: "relative",
+    marginBottom: "-3.5px",
+  },
 };
 function MiniPalette(props) {
-  const { classes, paletteName, emoji } = props;
+  const { classes, paletteName, emoji, colors } = props;
+  const miniColorBoxes = colors.map((color) => (
+    <div
+      className={classes.miniColor}
+      style={{ backgroundColor: color.color }}
+      key={color.name}
+    />
+  ));
+
   return (
     <div className={classes.root}>
-      <div className={classes.colors}></div>
+      <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName}
         <span className={classes.emoji}>{emoji}</span>
