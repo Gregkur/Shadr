0885ea843025c0a8f55c4682b860785e3fae43ca
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-01-06 16:06:00 +0100

    JSS styling higher order component"

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,18 +1,51 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/styles";
 import MiniPalette from "./MiniPalette";
 
-export default class PaletteList extends Component {
+const styles = {
+  root: {
+    backgroundColor: "blue",
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDiraction: "column",
+    flexWrap: "wrap",
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  palettes: {},
+};
+class PaletteList extends Component {
   render() {
-    const { palettes } = this.props;
+    const { palettes, classes } = this.props;
     return (
-      <div>
-        <h1>React Colors!</h1>
-        {palettes.map((palette) => (
-          <MiniPalette {...palette} />
-        ))}
-        {/* <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link> */}
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1>React Colors!</h1>
+          </nav>
+          <div className={classes.palettes}>
+            {palettes.map((palette) => (
+              <MiniPalette {...palette} />
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
+}
+export default withStyles(styles)(PaletteList);
+
+{
+  /* <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link> */
 }
