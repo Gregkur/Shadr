519404b694bac6143355df7cbfd70add87a9b813
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-01-06 10:13:34 +0100

    added material ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
 import Slider from "rc-slider";
+import Select from "@material-ui/core/Select";
+import { MenuItem } from "@material-ui/core";
 import "rc-slider/assets/index.css";
 import "../styles/Navbar.css";
 
@@ -21,6 +24,13 @@ export default class Navbar extends Component {
               step={100}
               onAfterChange={changeLevel}
             />
+          </div>
+          <div className="select-container">
+            <Select>
+              <MenuItem value="hex">HEX - #ffffff</MenuItem>
+              <MenuItem value="hex">RGB - rgb(255, 255, 255)</MenuItem>
+              <MenuItem value="hex">RGBA - rgba(255, 255, 255, 1.0</MenuItem>
+            </Select>
           </div>
         </div>
       </header>
