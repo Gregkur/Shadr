65c1a13868916a10c5f5166917f03b7b70d3ab75
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-01-06 18:18:01 +0100

    linked all shades to display correctly

--- src/components/SingleColorPalette.js
+++ src/components/SingleColorPalette.js
@@ -6,12 +6,14 @@ export default class SingleColorPalette extends Component {
   constructor(props) {
     super(props);
     this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+    this.state = { format: "hex" };
     console.log(this._shades);
   }
+
   gatherShades(palette, filteredColor) {
     let shades = [];
     let allColors = palette.colors;
-    //loop
+    //loop and match color
     for (let key in allColors) {
       shades = shades.concat(
         allColors[key].filter((color) => color.id === filteredColor)
@@ -20,11 +22,16 @@ export default class SingleColorPalette extends Component {
     //return given shades
     return shades;
   }
+
   render() {
+    const { format } = this.state;
+    const colorBoxes = this._shades.map((shade) => (
+      <ColorBox background={shade[format]} name={shade.name} key={shade.hex} />
+    ));
     return (
       <div className="Palette">
         <Navbar />
-        <div className="Palette-colors">$$$</div>
+        <div className="Palette-colors">{colorBoxes}</div>
         <footer className="Palette-footer">
           Color Name
           <span className="Palette-emoji">emoji</span>
