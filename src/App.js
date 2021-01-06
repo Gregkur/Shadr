bf814b43d908f24719c1811bd13c39483b2f227d
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-01-06 12:50:41 +0100

    added basic routing in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import Palette from "./components/Palette";
 import seedColors from "./seedColors";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { generatePalette } from "./colorHelpers";
 import "./App.css";
 
 function App() {
-  // console.log(generatePalette(seedColors[4]));
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[1])} />
-    </div>
+    <Router>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => <h1>This will be the main page</h1>}
+        />
+        <Route
+          exact
+          path="/palette/:id"
+          render={() => <h1>ONE ONE THE PALETTES</h1>}
+        />
+      </Switch>
+    </Router>
+    // <div>
+    //   <Palette palette={generatePalette(seedColors[1])} />
+    // </div>
   );
 }
 
