e74ac26e3fc270eb4aa6548e0702cd38cc676fbf
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-01-14 16:21:07 +0100

    add delete icon to mini pallet comp.

--- src/styles/MiniPaletteStyles.js
+++ src/styles/MiniPaletteStyles.js
@@ -7,11 +7,14 @@ const styles = {
     position: "relative",
     overflow: "hidden",
     transition: "transform 0.3s ease-in-out",
+    cursor: "pointer",
     "&:hover": {
       transform: "scale(1.1)",
-      cursor: "pointer",
       opacity: "0.9",
       scale: "1.2",
+      "& svg": {
+        opacity: 1,
+      },
     },
   },
   colors: {
@@ -39,6 +42,23 @@ const styles = {
     margin: "0 auto",
     position: "relative",
     marginBottom: "-4px",
+  },
+  delete: {},
+  deleteIcon: {
+    color: "white",
+    backgroundColor: "#eb3d30",
+    width: "20px",
+    height: "20px",
+    position: "absolute",
+    right: "0px",
+    top: "0px",
+    padding: "10px",
+    zIndex: "10",
+    opacity: "0",
+    transition: " all 0.3s ease-in-out",
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
   },
 };
 export default styles;
