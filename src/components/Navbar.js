553ff8f1f8fbe4fe76115fd75a9bd4e45fff0893
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-01-14 14:13:03 +0100

    refactored navbar css into jss

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { MenuItem, Snackbar } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/styles";
+import styles from "../styles/NavbarStyles";
 import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import "rc-slider/assets/index.css";
-import "../styles/Navbar.css";
 
-export default class Navbar extends Component {
+class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = { format: "hex", open: false };
@@ -28,17 +29,17 @@ export default class Navbar extends Component {
     });
   }
   render() {
-    const { level, changeLevel, sliderOpen } = this.props;
+    const { level, changeLevel, sliderOpen, classes } = this.props;
     const { format, open } = this.state;
     return (
-      <header className="Navbar">
-        <div className="logo">
+      <header className={classes.Navbar}>
+        <div className={classes.logo}>
           <Link to="/">React Palette</Link>
         </div>
         {sliderOpen && (
-          <div className="slider-container">
+          <div className={classes.sliderContainer}>
             <span>Level: {level}</span>
-            <div className="slider">
+            <div className={classes.slider}>
               <Slider
                 defaultValue={level}
                 min={100}
@@ -49,7 +50,7 @@ export default class Navbar extends Component {
             </div>
           </div>
         )}
-        <div className="select-container">
+        <div className={classes.selectContainer}>
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - #ffffff</MenuItem>
             <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
@@ -86,3 +87,5 @@ export default class Navbar extends Component {
 Navbar.defaultProps = {
   sliderOpen: true,
 };
+
+export default withStyles(styles)(Navbar);
